ff05bcbc6ea03bfaa6a85236c06fc348b114951c
Julio Apaza <julio7414.trading@gmail.com>
2024-10-02 21:47:37 -0500

    10-02-2024

--- lib/functions.hpp
+++ lib/functions.hpp
@@ -29,11 +29,13 @@ void arithmeticAverage ()
         cout << "No numbers entered." << endl;
     }
 }
-/*
+
 void weightedArithmeticMean ()
 {
     string sInput;
     float fInput;
+    string sWeight;
+    float fWeight;
     DynamicArray<float> floatArray;
     while (true) {
         cout << "Enter a number (or press Enter to finish): ";
@@ -43,21 +45,20 @@ void weightedArithmeticMean ()
             break;
         }
 
-        stringstream ss(sInput);
-        if (ss >> fInput) {
-            floatArray.addElement(fInput);
-        } else {
-            cout << "Invalid entry. Please enter a number." << endl;
-        }
-    }
+        cout << "Enter the weight (or press Enter to set default weight 1): ";
+        getline(cin, sWeight);
+
+        if (sWeight.empty()) {
+            fWeight = 1;
+        } // Inconplete
 
     if (floatArray.getSize() > 0) {
         float sum = 0;
         for (unsigned int i = 0; i < floatArray.getSize(); i++) {
             sum += floatArray[i];
         }
-        cout << "The arithmetic average is: " << sum / floatArray.getSize() << endl;
+        cout << "The weighted arithmetic mean is: " << sum / floatArray.getSize() << endl;
     } else {
         cout << "No numbers entered." << endl;
     }
-}*/
+}
